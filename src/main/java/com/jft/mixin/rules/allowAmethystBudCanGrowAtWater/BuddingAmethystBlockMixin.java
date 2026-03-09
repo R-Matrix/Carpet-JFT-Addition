3a08e9e8a16d45e82a3bbdea9a5b447593599c30
shudpaa393 <shudpaa393@163.com>
2026-03-09 16:28:32 +0800

    Fix: issue #1

--- src/main/java/com/jft/mixin/rules/allowAmethystBudCanGrowAtWater/BuddingAmethystBlockMixin.java
+++ src/main/java/com/jft/mixin/rules/allowAmethystBudCanGrowAtWater/BuddingAmethystBlockMixin.java
@@ -1,4 +1,4 @@
-package com.jft.mixin;
+package com.jft.mixin.rules.allowAmethystBudCanGrowAtWater;
 
 import com.jft.CarpetJFTSettings;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
--- src/main/java/com/jft/mixin/rules/impalingWaterContact/EnchantmentMixin.java
+++ src/main/java/com/jft/mixin/rules/impalingWaterContact/EnchantmentMixin.java
@@ -4,8 +4,6 @@ import com.jft.CarpetJFTSettings;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
-import com.llamalad7.mixinextras.sugar.Share;
-import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.Enchantments;
@@ -17,6 +15,7 @@ import net.minecraft.loot.context.LootContext;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.server.world.ServerWorld;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
@@ -26,16 +25,21 @@ import java.util.List;
 @Mixin(Enchantment.class)
 public abstract class EnchantmentMixin {
 
+
+    @Unique
+    private static final ThreadLocal<Boolean> IS_IMPALING_FLAG_REF = new ThreadLocal<>();
+    @Unique
+    private static final ThreadLocal<Entity> USER_REF = new ThreadLocal<>();
+
     @ModifyArg(method = "modifyValue(Lnet/minecraft/component/ComponentType;Lnet/minecraft/server/world/ServerWorld;ILnet/minecraft/item/ItemStack;Lnet/minecraft/entity/Entity;Lnet/minecraft/entity/damage/DamageSource;Lorg/apache/commons/lang3/mutable/MutableFloat;)V",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/enchantment/Enchantment;applyEffects(Ljava/util/List;Lnet/minecraft/loot/context/LootContext;Ljava/util/function/Consumer;)V"))
     private List<EnchantmentEffectEntry<EnchantmentValueEffect>> se
             (List<EnchantmentEffectEntry<EnchantmentValueEffect>> entries,
-             @Share("isImpalingFlag") LocalRef<Boolean> flagRef, @Share("beAttractedEntity") LocalRef<Entity> userRef,
              @Local(argsOnly = true) ServerWorld world, @Local(argsOnly = true) Entity user, @Local(argsOnly = true) ItemStack stack){
 
-        flagRef.set(stack != null && EnchantmentHelper.getLevel(
+        IS_IMPALING_FLAG_REF.set(stack != null && EnchantmentHelper.getLevel(
                 world.getRegistryManager().getOrThrow(RegistryKeys.ENCHANTMENT).getOrThrow(Enchantments.IMPALING), stack)> 0);
-        userRef.set(user);
+        USER_REF.set(user);
 
         return entries;
     }
@@ -44,13 +48,16 @@ public abstract class EnchantmentMixin {
             target = "Lnet/minecraft/enchantment/effect/EnchantmentEffectEntry;test(Lnet/minecraft/loot/context/LootContext;)Z"))
     private static boolean redirectTestMethod(
             EnchantmentEffectEntry<?> entries, LootContext lootContext,
-            Operation<Boolean> original, @Share("isImpalingFlag") LocalRef<Boolean> flagRef, @Share("beAttractedEntity") LocalRef<Entity> userRef) {
-        if (CarpetJFTSettings.impalingWaterContact && flagRef.get()){
-            flagRef.set(false);
-            if(userRef.get().isAlive()&& userRef.get().isTouchingWaterOrRain()){
+            Operation<Boolean> original) {
+        if (CarpetJFTSettings.impalingWaterContact && IS_IMPALING_FLAG_REF.get()){
+            if(USER_REF.get().isAlive()&& USER_REF.get().isTouchingWaterOrRain()){
                 return true;
             }
         }
+
+        IS_IMPALING_FLAG_REF.remove();
+        USER_REF.remove();
+
         return original.call(entries, lootContext);
     }
 }
